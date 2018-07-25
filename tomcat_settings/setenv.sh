27b632a6dce8750bf8a813cfb04088759c9d5641
Simone Lanucara <simonelanucara@yahoo.it>
2018-07-25 14:51:43 +0200

    Update setenv.sh

--- tomcat_settings/setenv.sh
+++ tomcat_settings/setenv.sh
@@ -1,1 +1,4 @@
-JAVA_OPTS="$JAVA_OPTS -Xmx1536M"
+#JAVA_OPTS="$JAVA_OPTS -Xmx1536M"
+ JAVA_OPTS="-Djava.awt.headless=true -server -Xms2G -Xmx4G -Xrs -XX:PerfDataSamplingInterval=500 \
+ -Dorg.geotools.referencing.forceXY=true -XX:SoftRefLRUPolicyMSPerMB=36000 -XX:+UseParallelGC -XX:NewRatio=2 \
+ -XX:+CMSClassUnloadingEnabled"
